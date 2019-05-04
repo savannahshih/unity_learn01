f8947fd3d91ccab88fcacca51cc73a6edfc7b241
s650089s <s650089s@gmail.com>
2019-05-04 18:05:14 +0800

    learn02 start

---
+++ unity_learn02_BulletWaltz/Assets/BulletScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+
+	private Rigidbody2D rigidbody2D;
+	private SpriteRenderer spriteRenderer;
+	private float speed = 5;
+
+	public void InitAndShoot(Vector2 direction)
+	{
+		rigidbody2D = this.GetComponent<Rigidbody2D>();
+		spriteRenderer = this.GetComponent<SpriteRenderer> ();
+
+		spriteRenderer.color = new Color (1.0f, 1.0f, 1.0f, 1.0f);
+		rigidbody2D.velocity = speed * direction;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
